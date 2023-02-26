d728d4b17673f0149e15edf5f90691ba8aada99c
RobinLB99 <RJLB991102@hotmail.com>
2023-02-26 12:57:16 -0500

    Initial commit

---
+++ biblioteca.js
@@ -0,0 +1,393 @@
+
+let biblioteca = [
+    {
+        title: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        year: 1967,
+        price: 15.99
+    },
+    {
+        title: "1984",
+        autor: "George Orwell",
+        year: 1949,
+        price: 12.99
+    },
+    {
+        title: "Matar a un ruiseñor",
+        autor: "Harper Lee",
+        year: 1960,
+        price: 14.99
+    },
+    {
+        title: "El gran Gatsby",
+        autor: "F. Scott Fitzgerald",
+        year: 1925,
+        price: 11.99
+    },
+    {
+        title: "Don Quijote de la Mancha",
+        autor: "Miguel de Cervantes",
+        year: 1605,
+        price: 18.99
+    },
+    {
+        title: "El Señor de los Anillos: La Comunidad del Anillo",
+        autor: "J.R.R. Tolkien",
+        year: 1954,
+        price: 20.99
+    },
+    {
+        title: "Orgullo y prejuicio",
+        autor: "Jane Austen",
+        year: 1813,
+        price: 10.99
+    },
+    {
+        title: "En busca del tiempo perdido: Por el camino de Swann",
+        autor: "Marcel Proust",
+        year: 1913,
+        price: 25.99
+    },
+    {
+        title: "El guardián entre el centeno",
+        autor: "J.D. Salinger",
+        year: 1951,
+        price: 13.99
+    },
+    {
+        title: "Las aventuras de Huckleberry Finn",
+        autor: "Mark Twain",
+        year: 1884,
+        price: 9.99
+    },
+    {
+        title: "El retrato de Dorian Gray",
+        autor: "Oscar Wilde",
+        year: 1890,
+        price: 12.99
+    },
+    {
+        title: "El nombre de la rosa",
+        autor: "Umberto Eco",
+        year: 1980,
+        price: 16.99
+    },
+    {
+        title: "Drácula",
+        autor: "Bram Stoker",
+        year: 1897,
+        price: 11.99
+    },
+    {
+        title: "Moby Dick",
+        autor: "Herman Melville",
+        year: 1851,
+        price: 14.99
+    },
+    {
+        title: "La naranja mecánica",
+        autor: "Anthony Burgess",
+        year: 1962,
+        price: 17.99
+    },
+    {
+        title: "El extranjero",
+        autor: "Albert Camus",
+        year: 1942,
+        price: 10.99
+    },
+    {
+        title: "El viejo y el mar",
+        autor: "Ernest Hemingway",
+        year: 1952,
+        price: 11.99
+    },
+    {
+        title: "La isla del tesoro",
+        autor: "Robert Louis Stevenson",
+        year: 1883,
+        price: 9.99
+    },
+    {
+        title: "La metamorfosis",
+        autor: "Franz Kafka",
+        year: 1915,
+        price: 8.99
+    },
+    {
+        title: "Ulises",
+        autor: "James Joyce",
+        year: 1922,
+        price: 22.99
+    },
+    {
+        title: "Crimen y castigo",
+        autor: "Fiódor Dostoyevski",
+        year: 1866,
+        price: 13.99
+    },
+    {
+        title: "Madame Bovary",
+        autor: "Gustave Flaubert",
+        year: 1856,
+        price: 11.99
+    },
+    {
+        title: "Los hermanos Karamazov",
+        autor: "Fiódor Dostoyevski",
+        year: 1880,
+        price: 16.99
+    },
+    {
+        title: "La guerra y la paz",
+        autor: "León Tolstói",
+        year: 1869,
+        price: 24.99
+    },
+    {
+        title: "La ciudad y los perros",
+        autor: "Mario Vargas Llosa",
+        year: 1963,
+        price: 15.99
+    },
+    {
+        title: "La tregua",
+        autor: "Mario Benedetti",
+        year: 1960,
+        price: 12.99
+    },
+    {
+        title: "Pedro Páramo",
+        autor: "Juan Rulfo",
+        year: 1955,
+        price: 11.99
+    },
+    {
+        title: "Rayuela",
+        autor: "Julio Cortázar",
+        year: 1963,
+        price: 18.99
+    },
+    {
+        title: "La casa de los espíritus",
+        autor: "Isabel Allende",
+        year: 1982,
+        price: 14.99
+    },
+    {
+        title: "El otoño del patriarca",
+        autor: "Gabriel García Márquez",
+        year: 1975,
+        price: 17.99
+    },
+    {
+        title: "La sombra del viento",
+        autor: "Carlos Ruiz Zafón",
+        year: 2001,
+        price: 19.99
+    },
+    {
+        title: "Crónica de una muerte anunciada",
+        autor: "Gabriel García Márquez",
+        year: 1981,
+        price: 10.99
+    },
+    {
+        title: "El amor en los tiempos del cólera",
+        autor: "Gabriel García Márquez",
+        year: 1985,
+        price: 13.99
+    },
+    {
+        title: "La casa de Bernarda Alba",
+        autor: "Federico García Lorca",
+        year: 1936,
+        price: 9.99
+    },
+    {
+        title: "La Regenta",
+        autor: "Leopoldo Alas Clarín",
+        year: 1884,
+        price: 12.99
+    },
+    {
+        title: "El laberinto de la soledad",
+        autor: "Octavio Paz",
+        year: 1950,
+        price: 9.99
+    },
+    {
+        title: "El Aleph",
+        autor: "Jorge Luis Borges",
+        year: 1949,
+        price: 7.99
+    },
+    {
+        title: "La muerte de Artemio Cruz",
+        autor: "Carlos Fuentes",
+        year: 1962,
+        price: 11.99
+    },
+    {
+        title: "La vorágine",
+        autor: "José Eustasio Rivera",
+        year: 1924,
+        price: 8.99
+    },
+    {
+        title: "Las batallas en el desierto",
+        autor: "José Emilio Pacheco",
+        year: 1981,
+        price: 10.99
+    },
+    {
+        title: "Los pasos perdidos",
+        autor: "Alejo Carpentier",
+        year: 1953,
+        price: 13.99
+    },
+    {
+        title: "Doce cuentos peregrinos",
+        autor: "Gabriel García Márquez",
+        year: 1992,
+        price: 14.99
+    },
+    {
+        title: "La fiesta del chivo",
+        autor: "Mario Vargas Llosa",
+        year: 2000,
+        price: 16.99
+    },
+    {
+        title: "La invención de Morel",
+        autor: "Adolfo Bioy Casares",
+        year: 1940,
+        price: 9.99
+    },
+    {
+        title: "Los detectives salvajes",
+        autor: "Roberto Bolaño",
+        year: 1998,
+        price: 19.99
+    },
+    {
+        title: "2666",
+        autor: "Roberto Bolaño",
+        year: 2004,
+        price: 22.99
+    },
+    {
+        title: "El túnel",
+        autor: "Ernesto Sabato",
+        year: 1948,
+        price: 11.99
+    },
+    {
+        title: "La casa verde",
+        autor: "Mario Vargas Llosa",
+        year: 1966,
+        price: 14.99
+    },
+    {
+        title: "La náusea",
+        autor: "Jean-Paul Sartre",
+        year: 1938,
+        price: 8.99
+    },
+    {
+        title: "La insoportable levedad del ser",
+        autor: "Milan Kundera",
+        year: 1984,
+        price: 12.99
+    },
+    {
+        title: "El amante de Lady Chatterley",
+        autor: "D.H. Lawrence",
+        year: 1928,
+        price: 9.99
+    },
+    {
+        title: "Los renglones torcidos de Dios",
+        autor: "Torcuato Luca de Tena",
+        year: 1979,
+        price: 7.99
+    },
+    {
+        title: "El médico",
+        autor: "Noah Gordon",
+        year: 1986,
+        price: 10.99
+    },
+    {
+        title: "El señor de las moscas",
+        autor: "William Golding",
+        year: 1954,
+        price: 8.99
+    },
+    {
+        title: "El resplandor",
+        autor: "Stephen King",
+        year: 1977,
+        price: 11.99
+    },
+    {
+        title: "El coronel no tiene quien le escriba",
+        autor: "Gabriel García Márquez",
+        year: 1961,
+        price: 9.99
+    },
+    {
+        title: "El proceso",
+        autor: "Franz Kafka",
+        year: 1925,
+        price: 7.99
+    },
+    {
+        title: "El dios de las pequeñas cosas",
+        autor: "Arundhati Roy",
+        year: 1997,
+        price: 14.99
+    },
+    {
+        title: "La broma infinita",
+        autor: "David Foster Wallace",
+        year: 1996,
+        price: 18.99
+    },
+    {
+        title: "El jardín de los senderos que se bifurcan",
+        autor: "Jorge Luis Borges",
+        year: 1941,
+        price: 6.99
+    },
+    {
+        title: "La historia interminable",
+        autor: "Michael Ende",
+        year: 1979,
+        price: 8.99
+    },
+    {
+        title: "Mujercitas",
+        autor: "Louisa May Alcott",
+        year: 1868,
+        price: 7.99
+    },
+    {
+        title: "Fahrenheit 451",
+        autor: "Ray Bradbury",
+        year: 1953,
+        price: 9.99
+    },
+    {
+        title: "La catedral del mar",
+        autor: "Ildefonso Falcones",
+        year: 2006,
+        price: 12.99
+    },
+    {
+        title: "El perfume",
+        autor: "Patrick Süskind",
+        year: 1985,
+        price: 10.99
+    }
+]
---
+++ script.js
@@ -0,0 +1,129 @@
+document.addEventListener("DOMContentLoaded", () => {
+    class libro {
+      constructor(title, autor, year, price) {
+        this.title = title;
+        this.autor = autor;
+        this.year = year;
+        this.price = price;
+      }
+    }
+
+    let respuesta = document.querySelector("#respuesta")
+
+    document.getElementById("ingresar").addEventListener("click", () => {
+
+      respuesta.innerHTML = ""
+
+      let form = document.createElement("form")
+      form.id = "formulario"
+
+      let title = document.createElement("input"),
+          autor = document.createElement("input"),
+          year = document.createElement("input"),
+          price = document.createElement("input"),
+          ingresar = document.createElement("input")
+
+      title.type = "text";
+      title.placeholder = "Ingresa el Titulo del libro";
+      title.value = null
+
+      autor.type = "text";
+      autor.placeholder = "Ingresa el Autor del libro";
+      autor.value = null
+
+      year.type = "number";
+      year.placeholder = "Ingresa el año de publicacion del libro";
+      year.value = null
+
+      price.type = "number";
+      price.placeholder = "Ingresa el precio del libro";
+      price.value = null
+
+      ingresar.type = "button";
+      ingresar.value = "Ingresar libro";
+
+      form.appendChild(title);
+      form.appendChild(autor);
+      form.appendChild(year);
+      form.appendChild(price);
+      form.appendChild(ingresar);
+
+      respuesta.appendChild(form)
+
+      ingresar.addEventListener("click", () => {
+        let titleValue = title.value
+        let autorValue = autor.value
+        let yearValue = year.value
+        let priceValue = price.value
+
+        if (!titleValue || !autorValue || !yearValue || !priceValue) {
+          let existeHijo = document.querySelector("#respuesta #sinLlenar");
+
+          if (existeHijo) {
+            let alert = document.getElementById("sinLlenar")
+            alert.remove()
+          }
+
+          let alert = document.createElement("p")
+          alert.textContent = "Existen campos sin llenar"
+          alert.id = "sinLlenar"
+          respuesta.appendChild(alert)
+
+          if (!titleValue) {
+            title.focus()
+          } else if (!autorValue) {
+            autor.focus()
+          } else if (!yearValue) {
+            year.focus()
+          } else if (!priceValue) {
+            price.focus()
+          }
+
+        } else {
+          biblioteca.push(new libro(titleValue, autorValue, yearValue, priceValue));
+          console.log(biblioteca);
+
+          respuesta.innerHTML = ""
+          let ingresado = document.createElement("p")
+          ingresado.id = "ingresado"
+          ingresado.textContent = "Libro ingresado con exito!"
+          respuesta.appendChild(ingresado)
+
+        }
+
+      })
+
+    });
+
+    document.getElementById("ver").addEventListener("click", () => {
+
+      respuesta.innerHTML = ""
+      biblioteca.forEach(libro => {
+        // Crear el elemento div para el libro
+        let divLibro = document.createElement("div");
+        divLibro.classList.add("libro");
+
+        // Crear los elementos p para la clave y valor de cada propiedad
+        for (var clave in libro) {
+          let pClave = document.createElement("p");
+          pClave.classList.add("clave");
+          pClave.textContent = clave.toString().toUpperCase() + ": ";
+
+          let pValor = document.createElement("p");
+          pValor.classList.add("valor");
+          pValor.textContent = libro[clave];
+
+          // Anidar los elementos p dentro del divLibro
+          divLibro.appendChild(pClave);
+          divLibro.appendChild(pValor);
+        }
+
+        // Añadir el divLibro al contenedor
+        respuesta.appendChild(divLibro);
+      })
+    })
+
+    document.getElementById("clean").addEventListener("click", () => {
+      respuesta.innerHTML = ``
+    })
+});
